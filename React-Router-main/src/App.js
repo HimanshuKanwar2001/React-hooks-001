0d8031f6e04b4510eddc3a66287b35c297329d5f
HimanshuKanwar2001 <himanshukanwar2001@gmail.com>
2024-07-21 19:25:55 +0530

    creating routes in E-Learning App

--- React-Router-main/src/App.js
+++ React-Router-main/src/App.js
@@ -1,15 +1,28 @@
 import Hero from "./pages/app/hero/Hero";
 import Nav from "./components/nav/Nav";
 import Courses from "./pages/app/courses/Courses";
-
+import { BrowserRouter,createBrowserRouter,RouterProvider } from "react-router-dom";
+import Details from "./pages/app/details/Details";
 function App() {
-  return (
-    <div className="App">
-      <Nav />
-      <Hero />
-      <Courses />
-    </div>
-  );
+  
+      const browserRouter=createBrowserRouter([
+        {
+          path:'/',
+          element:<Nav/>,
+          children:[
+            {index:true,element:<Hero/>},
+            {
+              path:'/courses',children:[
+                {index:true, element:<Courses/>},
+                {path:"/:courseId", element:<Details/>}
+              ]
+            },
+          ]
+        },
+      ]);
+   return(
+    <RouterProvider router={browserRouter}/>
+   )
 }
 
 export default App;
