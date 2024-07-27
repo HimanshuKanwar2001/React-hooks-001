908dc42daff3c4507c7ff06fa1aea4b2b4cab297
himanshu kanwar <himanshukanwar2001@gmail.com>
2024-07-27 16:56:55 +0530

    added clor-picker

--- React-Router-main/src/App.js
+++ React-Router-main/src/App.js
@@ -1,28 +1,44 @@
 import Hero from "./pages/app/hero/Hero";
 import Nav from "./components/nav/Nav";
 import Courses from "./pages/app/courses/Courses";
-import { BrowserRouter,createBrowserRouter,RouterProvider } from "react-router-dom";
+import Chapter from "./pages/app/chapter/Chapter";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Details from "./pages/app/details/Details";
+import Learn from "./pages/app/learn/Learn";
+import Page404  from './pages/misc/Page404/Page404';
 function App() {
-  
-      const browserRouter=createBrowserRouter([
+  const browserRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Nav />,
+      errorElement:<Page404/>,
+      children: [
+        { index: true, element: <Hero /> },
         {
-          path:'/',
-          element:<Nav/>,
-          children:[
-            {index:true,element:<Hero/>},
-            {
-              path:'/courses',children:[
-                {index:true, element:<Courses/>},
-                {path:"/:courseId", element:<Details/>}
-              ]
-            },
-          ]
+          path: "/courses",
+          children: [
+            { index: true, element: <Courses /> },
+            { path: ":courseId", element: <Details /> },
+          ],
         },
-      ]);
-   return(
-    <RouterProvider router={browserRouter}/>
-   )
+        {
+          path: "/learn/:courseId",
+          element: <Learn />,
+          children: [
+            {
+              path: "chapter/:chapterId",
+              element: <Chapter />,
+            },
+          ],
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={browserRouter} />;
 }
 
 export default App;
